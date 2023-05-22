a23f7e3650151e1888325b18423fcb6238f2e323
João Pinheiro <jv.dev.systems@gmail.com>
2023-05-22 11:00:07 -0300

    Parse webhook info

--- src/app/api/webhooks/test/route.ts
+++ src/app/api/webhooks/test/route.ts
@@ -1,3 +1,4 @@
+import { MessageSentStatus } from "@prisma/client";
 import prisma from "../../../../../lib/prisma";
 import url from "url";
 
@@ -21,14 +22,42 @@ export async function POST(
         },
       },
     });
+    const entries = body.entry;
+    const promises = entries.map(async (entry: any) => {
+      const id = entry.id;
+      const promises = entry.changes.map(async (change: any) => {
+        const field = change.field;
+        const value = change.value;
+        const isStatusUpdate = !!value.statuses;
 
-    // Parse the request body from the POST
-    console.log(`${new Date()} WHATS WEBHOOK AND CHANGES PUTTIN DO DDB`);
-    console.log(body);
-    console.log(JSON.stringify(body));
+        if (isStatusUpdate) {
+          const data: Omit<MessageSentStatus, "id"> = {
+            conversationId: "",
+            phone: "",
+            customerPhone: "",
+            status: "",
+          };
+          data.phone = value.metadata.display_phone_number;
+          value.statuses.forEach((status: any) => {
+            data.customerPhone = status.recipient_id;
+            data.conversationId = status.id;
+            data.status = status.status;
+          });
+          await prisma.messageSentStatus.create({ data });
+        } else {
+        }
+      });
+      await Promise.all(promises);
+    });
+    await Promise.all(promises);
     return new Response("Teste", { status: 200 });
   } catch (err) {
-    return new Response(JSON.stringify({ error: err }), { status: 400 });
+    if (err instanceof Error) {
+      await prisma.errors.create({
+        data: { error: err.stack },
+      });
+    }
+    return new Response(JSON.stringify({ error: err }), { status: 500 });
   }
 }
 
